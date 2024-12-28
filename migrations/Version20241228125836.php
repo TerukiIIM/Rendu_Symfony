48fe10653ba3f670506836c1a1dac111a691730d
TerukiTogami <147390861+TerukiIIM@users.noreply.github.com>
2024-12-28 17:11:51 +0100

    Systems

---
+++ migrations/Version20241228125836.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20241228125836 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add created_at column to user table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP created_at');
+    }
+}
